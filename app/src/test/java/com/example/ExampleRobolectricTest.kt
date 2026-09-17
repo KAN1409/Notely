35afcdf29cac028418988d758173560e28e69f07
KAN1409 <engkareemnasser@gmail.com>
2026-09-17 09:09:00 +0300

    test: align app name assertion with current product identity

--- app/src/test/java/com/example/ExampleRobolectricTest.kt
+++ app/src/test/java/com/example/ExampleRobolectricTest.kt
@@ -19,7 +19,7 @@ class ExampleRobolectricTest {
   fun `read string from context`() {
     val context = ApplicationProvider.getApplicationContext<Context>()
     val appName = context.getString(R.string.app_name)
-    assertEquals("Notely", appName)
+    assertEquals("NexNote", appName)
   }
 
   @Test
